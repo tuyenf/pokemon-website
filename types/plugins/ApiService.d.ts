4ae963fff69ce393fac9cc25abad9f064d11dfb9
tuyenf <tuyenntb@ycomm.vn>
2024-05-18 13:03:39 +0700

    Remove types folder

--- types/plugins/ApiService.d.ts
+++
@@ -1,8 +0,0 @@
-import { AxiosInstance } from 'axios';
-export declare class ApiService {
-    private static _instance;
-    private readonly _api;
-    constructor();
-    static instance(): ApiService;
-    get axios(): AxiosInstance;
-}
--- types/plugins/Constant.d.ts
+++
@@ -1,9 +0,0 @@
-export declare enum Constant {
-    POKEMON_LIST = "pokemons",
-    POKEMON_TYPES = "types",
-    POKEMON_DETAIL = "pokemons"
-}
-export declare const SortType: {
-    key: string;
-    name: string;
-}[];
