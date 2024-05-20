d68b0fb186147ab4689d5afa463f6cf05a936336
tuyenf <tuyenntb@ycomm.vn>
2024-05-20 09:03:55 +0700

    Remove all .vue.d.ts files

--- src/views/pokemons/TheFilter.vue.d.ts
+++
@@ -1,8 +0,0 @@
-declare const _default: import("vue").DefineComponent<{}, {}, {}, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {
-    selectedType: (value: number | null) => void;
-    selectedSort: (value: string) => void;
-}, string, import("vue").PublicProps, Readonly<import("vue").ExtractPropTypes<{}>> & {
-    onSelectedType?: ((value: number | null) => any) | undefined;
-    onSelectedSort?: ((value: string) => any) | undefined;
-}, {}, {}>;
-export default _default;
